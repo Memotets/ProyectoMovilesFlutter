5b816116d1dc8486e8bba49ce5dca76b8101520c
AlexisHS458 <47467314+AlexisHS458@users.noreply.github.com>
2019-11-26 19:41:34 -0600

    Procedencia agregada

--- proyect/lib/screens/registroAspirante.dart
+++ proyect/lib/screens/registroAspirante.dart
@@ -5,13 +5,13 @@ import 'package:proyect/models/Aspirante.dart';
 
 class registroAspirante extends StatefulWidget{
    Base base;
-	registroAspirante(Base base) {this.base = base;}
- 
+ 	 registroAspirante(Base base) {this.base = base;}
   @override
   State<StatefulWidget> createState() => new RegistroState();
 }
 
 class RegistroState extends State<registroAspirante>{
+  Base base;
   final apellidodos =TextEditingController();
   final apellidouno =TextEditingController();
   final correo =TextEditingController();
@@ -19,12 +19,13 @@ class RegistroState extends State<registroAspirante>{
   final nombre =TextEditingController();
   final sexo =TextEditingController();
   final telefono =TextEditingController();
-  //Base base;  
-  var pn, pa, sa,age,phone, email ,s, o,oo,ooo,unoopc,dosopc,tresopc,sex;
+  List<String> esc =["Preparatoria Independencia de Río Grande","Juana de Arco","Colegio del Centro","Escuela Prep Instituto Zacatecas","Instituto Tecnológico y de Estudios Superiores de Monterrey Campus Zacatecas","Preparatoria UAF","Preparatoria Vetagrande","Instituto Pedagógico J. Peaget","Instituto Educativo de Zacatecas","Colegio Federico Froebel","Centro Escolar Lancaster de Zacatecas","Universidad Autónoma de Durango Campus Zacatecas","Lic. José Minero Roque","Preparatoria Comercial Sor Juana Inés de La Cruz","Academia Comercial Remington","Preparatoria San Matías","Instituto Maxwell","Colegio Juan Pablo Ii","Colegio Daniel Márquez Medina A.C.","Colegio Santa Elena","Instituto de Formación Integral de Zacatecas","Escuela Preparatoria Particular Ramón  López Velarde","Instituto Ausubel","Instituto Educativo Nochistlan","Liceo E.S.L. Guadalupe","Liceo Universitario Guadalupe","Instituto del Carmen de Guadalupe","Instituto de Educación MONREAL Sandoval","CONSEJO Educativo de Zacatecas","Colegio Juan Pablo Ii El Grande","Superior Teaching Preparatoria","Bachillerato General Sierra Madre","Instituto Educativo de Calera","Instituto Asis de Guadalupe","Colegio Río Grande","Instituto Edison Ac","Cultura  Educación de Fresnillo Asociación Civil","Centro de Servicios de Educación Media Superior A Distancia"," El Cazadero","Educación Media Superior A Distancia","Profr. José Santos Valdez","Lic. Mauricio Magdaleno","José G Montes","Profa. Ma de La O Marín Mota","Francisco García Salinas","Escuela Preparatoria José Rodríguez Elías","Valentín Gómez Farías","Prof. Salvador Vidal G","González Ortega","Ing.  Gral. Felipe B. Berriozabal","Gral Lazaro Cardenas","Victor Rosales","Preparatoria Villanueva","Candelario Huizar","Panfilo Natera","Lazaro Cardenas del Río","José Vasconcelos","Beatriz Márquez Acosta","Ramón  López Velarde","Adolfo López Mateos","Jaime Torres Bodet","Tayahua","Genaro Codina","Lic. Agustín Yañez","Felix U Gómez","Francisco Goitia","Jesús Reyes Heroles","Daniel Camarena","Genaro Codina","Eulalio Gutiérrez","Salvador Varela Resendiz","Colegio de Bachilleres ZACATECAS","Colegio de Bachilleres","Colegio de Bachilleres Ojocaliente","Colegio de Bachilleres Ojocaliente","Colegio de Bachilleres Sain Alto","Colegio de Bachilleres Plantel Cañitas de Felipe Pescador","Luis de La Rosa Oteiza","Colegio de Bachilleres Plantel Sombrerete","Colegio de Bachilleres Victor Rosales","Colegio de Bachilleres Roberto Cabral del Hoyo","Colegio de Bachilleres Plantel EL Fuerte","Colegio de Bachilleres Plantel Trancoso"," Colegio de Bachilleres Plantel Chaparrosa","Colegio de Bachilleres Plantel Los Campos","Preparatoria Núm. 1","Preparatoria Núm. 2","Preparatoria Núm. 3","Preparatoria Núm. 4","Preparatoria Núm. 5","Preparatoria Núm. 6","Preparatoria Núm. 7","Preparatoria Núm. 8","Preparatoria Núm. 9","Preparatoria Núm. 10","Preparatoria Núm. 11","Preparatoria Núm. 12","Preparatoria Núm. 13","Colegio de Estudios Científicos y Tecnológicos del Estado Plantel  Victor Rosales","Colegio de Estudios Científicos y Tecnológicos del Estado  Plantel Tlaltenango","Colegio de Estudios Científicos y Tecnológicos del Estado  Plantel Río Grande","Cecyte Plantel San José de Lourdes","Colegio de Estudios Científicos y Tecnológicos del Edo.","Centro de Estudios Tecnológicos Industriales y de Servicios Num.147","Centro de Estudios Tecnológicos Industriales y de Servicios Num.23","Centro de Estudios Tecnológicos Industriales y de Servicios Num.1","Centro de Estudios Tecnológicos Industriales y de Servicios Num.104","Centro de Estudios Tecnológicos Industriales y de Servicios Num.141","Centro de Estudios Tecnológicos Industriales y de Servicios Num.215","Centro de Estudios Tecnológicos Industriales y de Servicios Num.221","Centro de Estudios Tecnológicos Industriales y de Servicios Num.113","Centro de Estudios Tecnológicos Industriales y de Servicios Num.114","Centro de Bachillerato Tecnológico Agropecuario Núm. 166","Centro de Bachillerato Tecnológico Agropecuario Núm. 137","Centro de Bachillerato Tecnológico Agropecuario Núm. 167","Centro de Bachillerato Tecnológico Agropecuario Núm. 138","Centro de Bachillerato Tecnológico Agropecuario Núm. 188","Centro de Bachillerato Tecnológico Agropecuario Núm. 189","Centro de Bachillerato Tecnológico Agropecuario Núm. 20","Centro de Bachillerato Tecnológico Agropecuario Núm. 88","Centro de Bachillerato Tecnológico Agropecuario Núm. 285","Centro de Bachillerato Tecnológico Agropecuario Núm. 286","Instituto Universitario Metropolitano","Escuela de Enfermería  Beatriz González Ortega","Colegio de Educación Profesional Técnica del Estado de Zacatecas","Centro de Estudios Científicos y Tecnológicos 18 “Zacatecas”","COBAEE","Salvador Varela Resendiz","INSTITUTO UNIVERSITARIO DEL CENTRO DE MÉXICO","Colegio de bachilleres plantel villa de cos","Instituto Politécnico Nacional","CECyTEZ Tlaltenango","Universidad autónoma de zacatecas preparatoria #4","Universidad Autónoma de Zacatecas Plantel IV","Instituto Miguel Agustín Pro","Unidad Académica Preparatoria Programa IV","Universidad Autonoma de Zacatecas Unidad Academica Preparatora 4","Educación Media Superior a Distancia 'Ramon Lopez Velarde'","EMSaD CECYTEZ EL RUCIO VILLA DE COS ZACATECAS","Preparatoria Estatal Víctor Rosales","Preparatoria #1 UAZ","Universidad Autónoma de Zacatecas Programa 1","Roberto Cabral del Hoyo Cobaez","Cbta 138","Universidad Autónoma de Zacatecas programa IV"];
+  var pn, pa, sa,age,phone, email,proce,procedencia ,s, o,oo,ooo,unoopc,dosopc,tresopc,sex;
    
   final formKey = new GlobalKey<FormState>();
   final scaffoldKey = new GlobalKey<ScaffoldState>();
- 
+  
+  @override
   Widget build(BuildContext context){
     return Scaffold(
         key: scaffoldKey,
@@ -62,8 +63,7 @@ class RegistroState extends State<registroAspirante>{
 								 },
             ),
             SizedBox(height: 15.0),
-            TextFormField(
-              
+            TextFormField(              
               controller: apellidouno,
               keyboardType: TextInputType.text,
               decoration: new InputDecoration(labelText: "Primer apellido", prefixIcon: Icon(Icons.person),
@@ -72,8 +72,7 @@ class RegistroState extends State<registroAspirante>{
                  focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Color.fromARGB(450, 107, 23, 64))),
               contentPadding: EdgeInsets.fromLTRB(20, 20, 20, 15),
                 border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
-                filled: true,
-                
+                filled: true,               
                 fillColor: Colors.grey[70],),
               validator: (val)=> val.length == 0 ? "Ingresa tu primer apellido":null,
               onChanged: (str){
@@ -170,20 +169,41 @@ class RegistroState extends State<registroAspirante>{
 								 	this.phone= str;       
 								 },              
             ),
-             SizedBox(height: 15.0),          
-            DropdownButtonFormField<String>(              
-                decoration: new InputDecoration(
-                   enabledBorder: OutlineInputBorder(
+            SizedBox(height: 15.0),
+             DropdownButtonFormField<String>(
+                decoration: new InputDecoration(labelText: "Preparatoria de procedencia", prefixIcon: Icon(Icons.school),
+                border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+                
+                 enabledBorder: OutlineInputBorder(
                  borderSide: BorderSide(color: Color.fromARGB(450, 107, 23, 64),), ),
                  focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Color.fromARGB(450, 107, 23, 64))),
-                labelText: "Primera opcion",    
-                prefixIcon: Icon(Icons.bookmark_border),           
-                contentPadding: EdgeInsets.fromLTRB(20, 20, 20, 15),
-                border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
                 filled: true,
-                fillColor: Colors.grey[70],
-                ),
-                value: o,
+                fillColor: Colors.grey[70],),       
+                  value: proce,
+                  
+                  items: esc.map((label)=> DropdownMenuItem(      
+              child: new SizedBox(
+                        width: 200.0,
+                        child: Text(label.toString(),overflow: TextOverflow.ellipsis,),
+                      ),
+              value: label)).toList(),
+                 onChanged: (value){
+                    setState(() {
+                this.proce = value;
+                this.procedencia=value;
+              });
+            },
+            ),  
+             SizedBox(height: 15.0),  
+             DropdownButtonFormField<String>(
+                decoration: new InputDecoration(labelText: "Primera opción", prefixIcon: Icon(Icons.bookmark_border),
+                border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+                 enabledBorder: OutlineInputBorder(
+                 borderSide: BorderSide(color: Color.fromARGB(450, 107, 23, 64),), ),
+                 focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Color.fromARGB(450, 107, 23, 64))),
+                filled: true,
+                fillColor: Colors.grey[70],),       
+                  value: o,
                 items:  ["Sistemas computacionales","Ambiental","Mecatrónica","Alimentos","Metalúrgica"].map((label)=> DropdownMenuItem(
               child: Text(label.toString()),
               value: label)).toList(),
@@ -193,7 +213,7 @@ class RegistroState extends State<registroAspirante>{
                 this.o=value;
               });
                   },
-            ),
+            ),        
              SizedBox(height: 15.0),
             DropdownButtonFormField<String>(
                 decoration: new InputDecoration(labelText: "Segunda opción", prefixIcon: Icon(Icons.bookmark_border),
@@ -249,14 +269,6 @@ class RegistroState extends State<registroAspirante>{
                 ),
           ),
         ),
-           /* new Container(
-              margin: const EdgeInsets.only(top: 10),
-              child: new RaisedButton(
-                color: Colors.teal[200],
-                onPressed: submitAspirante,
-                child: Text('Agregar aspirante'),
-              ),
-            )*/
           ]),
         ),
         )
@@ -269,11 +281,13 @@ class RegistroState extends State<registroAspirante>{
      String mensaje="";
      DateTime now = new DateTime.now();
      DateTime date = new DateTime(now.year, now.month, now.day);
-     String fech=date.toString();
+     String fech=date.day.toString()+"-"+date.month.toString()+"-"+date.year.toString();
+    var ed= int.parse(age);
 
      if(nombre.text.length >2 && apellidouno.text.length>2 
-     && edad.text.length > 1 && telefono.text.length==10 
+     && edad.text.length > 0 && telefono.text.length==10 
      && correo.text.length>1){
+       if( ed >10 && ed<50){
        if(o!=oo && ooo!=o && ooo!=oo){
           Aspirante asp =new Aspirante();
           var base = new Base();
@@ -284,15 +298,20 @@ class RegistroState extends State<registroAspirante>{
           asp.sexo=sex;
           asp.correo=email;
           asp.telefono=phone;
-          asp.procedencia="";
+          asp.procedencia=procedencia;
           asp.primerO=unoopc;
           asp.segundaO=dosopc;
           asp.tercerO=tresopc;
           asp.fecha=fech;
+          print(fech);
           int a = await base.addAspirante(asp);
           mensaje="Aspirante agregado";
-       }else{
+          print(asp);
+       }   else{
          mensaje="Revisa tus opciones de carrera";
+       }
+       }else{
+         mensaje="Ingresa una edad válida";
        }
 
      }else{
@@ -304,12 +323,5 @@ class RegistroState extends State<registroAspirante>{
 
 }
 
-  /*String fechear(DateTime d){
-     String y = d.;
-      String y = _fourDigits(year);
-    String m = _twoDigits(month);
-    String d = _twoDigits(day);
-    return
-  }*/
 
 }
